3c7a82af2e67b0749d10bda193971a3d28133a8b
RicoAchmad <rico_250@smkassalaambandung.sch.id>
2022-01-22 08:41:22 +0700

    Updated

--- latihan/latihan1.dart
+++ latihan/latihan1.dart
@@ -1,8 +1,14 @@
+import 'dart:io';
 void main() {
-  var jurusan = "RPL";
-  var kelas = 10;
+
+    stdout.write("Masukan Jurusan: ");
+    String? jurusan = stdin.readLineSync();
+
+    stdout.write("Masukan Kelas: ");
+    int? kelas = int.parse(stdin.readLineSync()!);
+
   if (jurusan == "RPL") {
-      if (kelas == 10){
+      if (kelas == 10) {
           print("Ini Kelas 10 RPL");
       } else if (kelas == 11) {
         print("Ini Kelas 11 RPL");
@@ -11,7 +17,27 @@ void main() {
       } else {
         print("Kelas Tidak Ada");
       }
-  } else {
-    print("Jurusan Tidak Ada");
-  }
+  } else if (jurusan == "TKRO"){
+    if (kelas == 10){
+      print("Ini Kelas 10 TKRO");
+    } else if (kelas == 11) {
+      print("Ini Kelas 11 TKRO");
+    } else if (kelas == 12) {
+      print("Ini Kelas 12 TKRO");
+    } else {
+      print("Kelas Tidak Ada");
+    }
+  } else if (jurusan == "TBSM"){
+    if (kelas == 10){
+      print("Ini Kelas 10 TBSM");
+    } else if (kelas == 11) {
+      print("Ini Kelas 11 TBSM");
+    } else if (kelas == 12) {
+      print("Ini Kelas 12 TBSM");
+    } else {
+      print("Kelas Tidak Ada");
+    }
+} else {
+ print("Jurusan Tidak Ada");
+}
 }
